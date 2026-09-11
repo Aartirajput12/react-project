0dcecdf68e9b110afae08f123e222f58519068ec
Naina <ns507100@gmail.com>
2026-09-12 00:37:46 +0530

    change

--- src/commerce/Img.jsx
+++ src/commerce/Img.jsx
@@ -1,14 +1,30 @@
-
 import React from 'react'
 
 const Img = () => {
   return (
     <>
-<style>{` .fashion-img { margin-top: 20px; width: 100%; height: 500px; object-fit: cover; display: block; } /* Mobile */ @media (max-width: 576px) { .fashion-img { height: 600px; } } /* Small mobile */ @media (max-width: 375px) { .fashion-img { height: 550px; } } `}</style>
+      <style>{`
+        .fashion-img {
+          margin-top: 20px;
+          width: 100%;
+          height: auto;
+          max-width: 100%;
+          display: block;
+        }
+
+        /* Mobile */
+        @media (max-width: 576px) {
+          .fashion-img {
+            width: 100%;
+            height: 250px;
+            object-fit: contain;
+          }
+        }
+      `}</style>
 
       <img
         src="https://images-static.nykaa.com/uploads/aa462a1e-cccc-4028-a12a-df50effabb3c.jpg?tr=cm-pad_resize,w-1800"
-  className="fashion-img"
+        className="fashion-img"
         alt="Women Fashion"
       />
     </>
@@ -16,4 +32,3 @@ const Img = () => {
 }
 
 export default Img
-
